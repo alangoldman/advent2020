8d4684c78829ca3011acd4553c2d4871c138dcb4
Alan Goldman <alan.goldman@gatech.edu>
2021-01-02 11:55:42 -0500

    day19 solved

--- day19.py
+++ day19.py
@@ -1,4 +1,4 @@
-file = open("day19_input.example2.txt", "r")
+file = open("day19_input.txt", "r")
 lines = [l.rstrip() for l in file.readlines()]
 file.close()
 
@@ -6,30 +6,35 @@ rules = {}
 def test(s, rule=None):
     if rule is None:
         rule = rules['0']
-    print(s, rule)
+    #print(s, rule)
     
     if s == '':
-        return (False, None)
+        return (False, [])
 
     if rule.startswith('"'):
-        return (s[0] == rule[1], s[1:])
+        return (s[0] == rule[1], [s[1:]])
 
     if '|' in rule:
         subrules = rule.split(' | ')
+        matches = []
         for subrule in subrules:
             subrule_test = test(s, subrule)
             if subrule_test[0]:
-                return (True, subrule_test[1])
-        return (False, None)
+                matches += subrule_test[1]
+        return (len(matches)>0, matches)
     else:
         subrules = rule.split(' ')
+        matches = [s]
         for subrule in subrules:
-            subrule_test = test(s, rules[subrule])
-            if subrule_test[0]:
-                s = subrule_test[1]
-            else:
-                return (False, None)
-        return (True, s)
+            next_matches = []
+            for match in matches:
+                subrule_test = test(match, rules[subrule])
+                if subrule_test[0]:
+                    next_matches += subrule_test[1]
+            if len(next_matches) == 0:
+                return (False, [])
+            matches = next_matches
+        return (True, matches)
     
     
 rule_mode = True
@@ -38,18 +43,18 @@ for line in lines:
     if rule_mode:
         if line == '':
             rule_mode = False
-            rules['8'] = '42 | 42 42'
-            rules['11'] = '42 31 | 42 42 31 31'
+            rules['8'] = '42 | 42 42 | 42 42 42 | 42 42 42 42 | 42 42 42 42 42 | 42 42 42 42 42 42'
+            rules['11'] = '42 31 | 42 42 31 31 | 42 42 42 31 31 31 | 42 42 42 42 31 31 31 31 | 42 42 42 42 42 31 31 31 31 31 | 42 42 42 42 42 42 31 31 31 31 31 31'
             continue
         key, value = line.split(': ')
         rules[key] = value
     else:
-        line = 'aaaaabbaabaaaaababaa'
-        #line = 'aaaabbaaaabbaaa'
+        #line = 'aaaaabbaabaaaaababaa'
+        #line = 'babbbbaabbbbbabbbbbbaabaaabaaa'
         result = test(line)
-        if result[0] and result[1] == '':
+        if result[0] and '' in result[1]:
             print(line)
             matches += 1
-        break
+        #break
             
 print(matches)
