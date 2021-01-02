513d69c9dd3efd1c1084acc6cfd9173e6458d068
Alan Goldman <alan.goldman@gatech.edu>
2021-01-02 11:45:42 -0500

    19b cleanup

--- day19.py
+++ day19.py
@@ -1,28 +1,26 @@
-file = open("day19_input.txt", "r")
+file = open("day19_input.example2.txt", "r")
 lines = [l.rstrip() for l in file.readlines()]
 file.close()
 
 rules = {}
 def test(s, rule=None):
-    #print(s, rule)
     if rule is None:
         rule = rules['0']
+    print(s, rule)
+    
     if s == '':
-        return (False, '')
+        return (False, None)
 
     if rule.startswith('"'):
         return (s[0] == rule[1], s[1:])
 
     if '|' in rule:
-        subrule1, subrule2 = rule.split(' | ')
-        subrule1_test = test(s, subrule1)
-        subrule2_test = test(s, subrule2)
-        if subrule1_test[0]:
-            return (True, subrule1_test[1])
-        elif subrule2_test[0]:
-            return (True, subrule2_test[1])
-        else:
-            return (False, None)
+        subrules = rule.split(' | ')
+        for subrule in subrules:
+            subrule_test = test(s, subrule)
+            if subrule_test[0]:
+                return (True, subrule_test[1])
+        return (False, None)
     else:
         subrules = rule.split(' ')
         for subrule in subrules:
@@ -40,13 +38,18 @@ for line in lines:
     if rule_mode:
         if line == '':
             rule_mode = False
+            rules['8'] = '42 | 42 42'
+            rules['11'] = '42 31 | 42 42 31 31'
             continue
         key, value = line.split(': ')
         rules[key] = value
     else:
+        line = 'aaaaabbaabaaaaababaa'
+        #line = 'aaaabbaaaabbaaa'
         result = test(line)
         if result[0] and result[1] == '':
             print(line)
             matches += 1
+        break
             
 print(matches)
